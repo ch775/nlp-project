0bd20472b6e3544dd20d98abad4794ec0d3943b0
Christina Hebets <chebets@tucson.local>
2020-04-02 14:36:41 -0700

    First commit

---
+++ src/server/index.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+var AYLIENTextAPI = require('aylien_textapi');
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8082, function () {
+    console.log('Example app listening on port 8082!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})

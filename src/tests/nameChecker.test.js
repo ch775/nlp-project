290e0e8e0bbb23e4917a686e017c37011b6ee26e
Christina Hebets <chebets@tucson.local>
2020-04-07 09:51:45 -0700

    Added tests

---
+++ src/tests/nameChecker.test.js
@@ -0,0 +1,7 @@
+import { checkForName } from '../client/js/nameChecker';
+
+describe('checkForName', () => {
+  test('checkForName should be defined', () => {
+    expect(checkForName).toBeDefined();
+  });
+});
